769d14e2fccb68569034b6f32875f3532b05f2e3
JustasZaltauskas <zaltauskasjustas@gmail.com>
2017-05-15 10:52:50 +0300

    add default image on mobile, remove unused images, make header transparent

--- src/AppBundle/Resources/scripts/myscript.js
+++ src/AppBundle/Resources/scripts/myscript.js
@@ -1,8 +1,7 @@
-$(function () {
-    var $findPassion = $(".btn-findPassion");
+var $findPassion = $(".btn-findPassion");
 
+$(function () {
     $findPassion.on('click', function () {
-        console.log('works');
 
         $('html body').animate({
             scrollTop: $('#intro-newest').offset().top
@@ -17,6 +16,42 @@ $(function() {
         $('.video video').css('display', 'none');
         $('.video-fallback').css('display', 'block');
     } else {
+        $('.video video').css('display', 'block');
         $('.video-fallback').css('display', 'none');
     }
 });
+
+$(document).ready(function() {
+    checkSize();
+
+    $(window).resize(checkSize);
+
+});
+
+//Function to the css rule
+function checkSize(){
+    if (window.matchMedia('(max-width: 767px)').matches) {
+        $('.transparent-header').css('position', 'relative');
+
+        $('.topnavbar').css('backgroundColor', 'white');
+
+        $('.header-logo').addClass('header-logo--black');
+        $('.header-logo').removeClass('header-logo--white');
+
+        $('.myEventLink').addClass('myEventLink--black');
+        $('.myEventLink--black').removeClass('myEventLink');
+
+        $('.navbar-nav').css('height', 'auto');
+    } else if ($('.intro-header').html()) {
+        $('.transparent-header').css('position', 'absolute');
+
+        $('.topnavbar').css('backgroundColor', 'transparent');
+
+        $('.header-logo').addClass('header-logo--white');
+        $('.header-logo').removeClass('header-logo--black');
+
+        $('.myEventLink--black').addClass('myEventLink');
+        $('.myEventLink').removeClass('myEventLink--black');
+
+    }
+}
