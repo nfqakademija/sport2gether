8b1ba159508f98ec98880c1cabdc0b38a972bab5
Pavelas Adomaitis <p.adomaitis@yahoo.com>
2017-05-18 14:46:43 +0300

    recapthca fixes

--- src/AppBundle/Resources/scripts/comment.js
+++ src/AppBundle/Resources/scripts/comment.js
@@ -1,14 +1,17 @@
 (function(){
     $('#commentSubmit').on('click',function(e){
+        var data = $('#myform').serialize();
         var comment = $('#comment').val();
         var id = $('#eventId').val();
         e.preventDefault();
         if(comment) {
             $.post({
                 url: "/addComment/" + id,
-                data: comment,
+                data: data,
                 error: function (err) {
-                    console.error(err);
+                    if(err.status==400){
+                        $('.js-verify').removeClass('hidden');
+                    };
                 },
                 success: function (data) {
                     location.reload();
